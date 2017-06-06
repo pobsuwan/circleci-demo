62674be29b8b5a51fc1c500ecf30e4510c46cad1
Apiwat Suksuwan <pob_suwan@hotmail.com>
2017-06-06 20:47:47 +0700

    Added plus(), minus(), multiply(), divide()

--- calc.js
+++ calc.js
@@ -1,3 +1,15 @@
 exports.plus = function plus(a, b) {
     return a + b;
 }
+
+exports.minus = function minus(a, b) {
+    return a - b;
+}
+
+exports.multiply = function multiply(a, b) {
+    return a * b;
+}
+
+exports.divide = function divide(a, b) {
+    return a / b;
+}
