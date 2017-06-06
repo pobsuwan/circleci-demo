72644d43a0bbfc1dd4da55563b6b78717985badf
Apiwat Suksuwan <pob_suwan@hotmail.com>
2017-06-06 20:51:16 +0700

    print example

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const calc = require('./calc');
 
 // Plus
-console.log(calc.plus(1,2));
+console.log("2 + 2 = " + calc.plus(2,2));
+console.log("2 - 2 = " + calc.minus(2,2));
+console.log("2 * 2 = " + calc.multiply(2,2));
+console.log("2 / 2 = " + calc.divide(2,2));
